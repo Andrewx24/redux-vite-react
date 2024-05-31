87e1f72649c1f4bcca6dd297726ce30dfc638427
Andrew Aliaj <andyaliaj@gmail.com>
2024-05-31 06:51:25 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+
+  return (
+    <>
+  <h1 className="text-3xl  text-center font-bold underline">
+      Hello world! Redux tutorial
+    </h1>
+  
+       
+    </>
+  )
+}
+
+export default App
